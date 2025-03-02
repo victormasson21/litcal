15c4f93ca320cf5f57c964478f2e3c6bdcbd87aa
Victor Masson <victormasson21@gmail.com>
2025-03-02 18:24:15 +0000

    Add secrets to deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+// TODO: separate local and production config
 const nextConfig: NextConfig = {
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
