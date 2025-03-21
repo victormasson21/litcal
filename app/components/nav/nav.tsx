28dbe34e79113bf2783f398473d00effefd0f61f
Victor Masson <victormasson21@gmail.com>
2025-03-21 19:47:23 +0000

    Refactor illustrations

--- app/components/nav/nav.tsx
+++ app/components/nav/nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./nav.module.css";
+import Image from "next/image";
 
 type Link = {
   url: string;
@@ -21,8 +22,13 @@ export const Navigation = ({ links }: { links: NavLinks }) => {
         <span>&larr;</span>
         <span>{left.text}</span>
       </Link>
-      <Link className={styles.link} href={center.url}>
-        <span>{center.text}</span>
+      <Link
+        className={styles.link}
+        href={center.url}
+        aria-label="Go back to the previous level (month or year)"
+      >
+        <Image src="/grid.svg" alt="Image of a grid" width={25} height={25} />
+        {/* <span>{center.text}</span> */}
       </Link>
       <Link className={styles.link} href={right.url}>
         <span>{right.text}</span>
--- app/components/seasons/seasons.tsx
+++ app/components/seasons/seasons.tsx
@@ -6,59 +6,53 @@ import snowflake from "./images/snowflake.png";
 import sun from "./images/sun.png";
 import styles from "./seasons.module.css";
 
-interface Season {
+export type SeasonKey = "winter" | "spring" | "summer" | "autumn";
+interface SeasonImage {
   className: string;
   alt: string;
   src: StaticImageData;
-  height: string;
-  transform: string;
 }
 
 type Props = {
-  seasons: Season[];
+  seasons: SeasonKey[];
 };
 
 export const Seasons = ({ seasons }: Props): ReactNode => (
   <div className={styles.container}>
-    {seasons.map(({ className, src, alt, height, transform }) => (
-      <Image
-        className={className && styles[className]}
-        key={alt}
-        src={src}
-        alt={alt}
-        style={{ height, transform, width: "fit-content" }}
-      />
-    ))}
+    {seasons.map((key) => {
+      const { className, src, alt } = seasonsData[key];
+      return (
+        <Image
+          className={className && styles[className]}
+          key={alt}
+          src={src}
+          alt={alt}
+          style={{ width: "fit-content" }}
+        />
+      );
+    })}
   </div>
 );
 
-export const seasonsData: Record<string, Season> = {
+export const seasonsData: Record<SeasonKey, SeasonImage> = {
   winter: {
     className: "winter",
     alt: "Etching of a snowflake",
     src: snowflake,
-    height: "90%",
-    transform: "rotate(10deg)",
   },
   spring: {
     className: "spring",
     alt: "Etching of a bud",
     src: smallbud,
-    height: "130%",
-    transform: "rotate(-30deg)",
   },
   summer: {
     className: "summer",
     alt: "Etching of the sun",
     src: sun,
-    height: "100%",
-    transform: "",
   },
   autumn: {
     className: "autumn",
     alt: "Etching of a chestnut leaf",
     src: leave,
-    height: "110%",
-    transform: "rotate(20deg)",
   },
 };
